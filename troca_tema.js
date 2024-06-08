c39b69714d3a48e5c734671131e1d7fa649c5f92
lippekstro <fellipecastro13@hotmail.com>
2024-06-08 11:14:03 -0300

    primeiro commit

---
+++ troca_tema.js
@@ -0,0 +1,30 @@
+const btn_tema = document.querySelector('#btn-tema')
+const html = document.querySelector('html')
+
+let estaEscuro = false
+
+btn_tema.addEventListener('click', ()=>{
+    if(estaEscuro){
+        html.setAttribute('tema', 'claro')
+        localStorage.setItem('modo', 'claro')
+        estaEscuro = false
+    } else {
+        html.setAttribute('tema', 'escuro')
+        localStorage.setItem('modo', 'escuro')
+        estaEscuro = true
+    }
+})
+
+let valor_atual = localStorage.getItem('modo')
+
+function trocaTemaAuto(valor_atual){
+    if(valor_atual == 'escuro') {
+        html.setAttribute('tema', 'escuro')
+        estaEscuro = true
+    } else {
+        html.setAttribute('tema', 'claro')
+        estaEscuro = false
+    }
+}
+
+trocaTemaAuto(valor_atual)
